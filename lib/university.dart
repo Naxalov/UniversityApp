dbd37e250e0a29c6203622ee35482040d90cc493
Naxalov <uzbzarif@gmail.com>
2020-02-05 23:18:32 +0900

    update University info

--- lib/university.dart
+++ lib/university.dart
@@ -1,11 +1,21 @@
 class University {
   final String name;
+  final String overview;
+  final String city;
+  final String acronym;
+  final String phone;
   final int id;
 
-  University({this.name, this.id});
+  University({this.name, this.overview, this.city, this.acronym, this.phone,
+      this.id});
+
   factory University.fromJSON(Map<String,dynamic> parsedJson){
     return University(
       name: parsedJson['name'],
+      overview: parsedJson['overview'],
+      city: parsedJson['city'],
+      acronym: parsedJson['acronym'],
+      phone: parsedJson['phone'],
       id: parsedJson['id']
     );
   }
