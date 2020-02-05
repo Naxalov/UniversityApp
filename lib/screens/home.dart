63ca905073ef604879952a632d0c333cad279f24
Naxalov <uzbzarif@gmail.com>
2020-02-05 21:27:53 +0900

    Dynamic ListView

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,30 +1,87 @@
 import 'package:flutter/material.dart';
 
+final europeanCountries = [
+  'Albania',
+  'Andorra',
+  'Armenia',
+  'Austria',
+  'Azerbaijan',
+  'Belarus',
+  'Belgium',
+  'Bosnia and Herzegovina',
+  'Bulgaria',
+  'Croatia',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Estonia',
+  'Finland',
+  'France',
+  'Georgia',
+  'Germany',
+  'Greece',
+  'Hungary',
+  'Iceland',
+  'Ireland',
+  'Italy',
+  'Kazakhstan',
+  'Kosovo',
+  'Latvia',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Macedonia',
+  'Malta',
+  'Moldova',
+  'Monaco',
+  'Montenegro',
+  'Netherlands',
+  'Norway',
+  'Poland',
+  'Portugal',
+  'Romania',
+  'Russia',
+  'San Marino',
+  'Serbia',
+  'Slovakia',
+  'Slovenia',
+  'Spain',
+  'Sweden',
+  'Switzerland',
+  'Turkey',
+  'Ukraine',
+  'Uzbekistan',
+  'United Kingdom',
+  'Vatican City'
+];
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('ListView'),),
-      body: myList(),
+      appBar: AppBar(
+        title: Text('ListView'),
+      ),
+      body: MyList(),
     );
   }
 }
 
-class myList extends StatelessWidget {
+class MyList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        ListTile(
-          title: Text('Sun'),
-        ),
-        ListTile(
-          title: Text('Moon'),
-        ),
-        ListTile(
-          title: Text('Star'),
-        )
-      ],
+    return ListView.separated(
+      itemCount: europeanCountries.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text(europeanCountries[index]),
+        );
+      }, separatorBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: EdgeInsets.symmetric(vertical: 0,horizontal: 15),
+          child: Divider(),
+        );
+    },
     );
   }
 }
