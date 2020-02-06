f5f3184d895ce56a5c4efe76f91afadcee25fbb7
Naxalov <uzbzarif@gmail.com>
2020-02-06 15:15:42 +0900

    update unversity list

--- lib/screens/detailScreen.dart
+++ lib/screens/detailScreen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:university_app/university.dart';
 
 class DetailScreen extends StatelessWidget {
   final University uni;
-  DetailScreen(University this.uni );
+  double boxHeight = 300;
+  double boxWidth = 450;
+
+  DetailScreen(University this.uni);
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +15,75 @@ class DetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(uni.name),
       ),
-      body: Text('Text'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Image.asset(
+              'assets/images/${uni.acronym}.jpg',
+              height: boxHeight,
+              width: boxWidth,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                uni.overview,
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+              child: Divider(),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.phone,
+                size: 20.0,
+                color: Colors.teal,
+              ),
+              title: Text(
+                '+82 (20) 880 4447',
+                style: GoogleFonts.sourceSansPro(
+                  color: Colors.teal[900],
+                  letterSpacing: 2.5,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.contact_mail,
+                size: 20.0,
+                color: Colors.teal,
+              ),
+              title: Text(
+                '+82 (54) 279 2099',
+                style: GoogleFonts.sourceSansPro(
+                  color: Colors.teal[900],
+                  letterSpacing: 2.5,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.add_location,
+                size: 20.0,
+                color: Colors.teal,
+              ),
+              title: Text(
+                '	98 Gunja-dong, Gwangjin-gu Seoul (population range: over 5,000,000)  143-747 Seoul   Korea',
+                style: GoogleFonts.sourceSansPro(
+                  color: Colors.teal[900],
+                  letterSpacing: 2.5,
+                  fontSize: 14.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
